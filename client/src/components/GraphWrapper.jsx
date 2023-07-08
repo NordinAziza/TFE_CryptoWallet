c27a5b25c5d05ca2fd88932089d0f3b42b5f7381
Nordin Aziza <coriolis.nordin@gmail.com>
2023-07-08 15:44:30 +0200

    'navBarFixed'

--- client/src/components/GraphWrapper.jsx
+++ client/src/components/GraphWrapper.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Graph from './Graph';
 import { useParams } from 'react-router-dom';
 
 const GraphWrapper = () => {
   const { coin } = useParams();
+  const [coinParam, setCoinParam] = useState(coin);
 
-  return <Graph coin={coin} />;
+  useEffect(() => {
+    setCoinParam(coin);
+  }, [coin]);
+
+  return <Graph key={coinParam} coin={coinParam} />;
 };
 
 export default GraphWrapper;
